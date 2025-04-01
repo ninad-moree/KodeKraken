28d82ab792ba741c76bef726b0b3602c8119ba24
Ninad More <ninadmore183@gmail.com>
2025-04-01 13:16:45 +0530

    Color added

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class ColorConstants {
   static const Color kPrimaryColor = Color(0xFF3B37FF);
   static const Color kBackgroundColor = Color(0xFFE3F2FD);
+
+  static const Color purple = Color(0xFF9360E2);
+  static const Color lightPurple = Color(0xFFC0A3EE);
 }
